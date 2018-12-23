55cf72f1509a754bd9d8ba9fdf663de717d78949
AbdulahHanafi <45525482+AbdulahHanafi@users.noreply.github.com>
2018-12-23 15:51:59 +0700

    Add files via upload

---
+++ menghitung E=mc2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int E,m,c;
+
+    cout << ("\t\t\Pelita bangsa\n\n") <<endl;
+    cout << ("========================================\n") <<endl;
+    cout << ("Nama : Abdulah Hanafi \nNIM : 311810100\n") <<endl;
+    cout << ("========================================\n") <<endl;
+
+    cout << ("\t    rumua E= m.c2\n") <<endl;
+    cout << "masukan nilai m :";
+    cin>>m;
+    cout << "masukan bilai c :";
+    cin>>c;
+
+    E=m*c*c;
+
+    cout <<"=================" <<endl;
+    cout << "jadi hasil nilai E adalah :"<<E<<"\n\n";
+
+    return 0;
+
+}
